55c3e3d6eeeee378053ff78d3a1f280dd8cb97cb
elliott <enix002@aucklanduni.ac.nz>
2019-10-26 10:23:43 +1300

    added setup of tables to sql file. added db file.

--- sql/project-database-init-script.sql
+++ sql/project-database-init-script.sql
@@ -4,13 +4,45 @@
  * required.
  */
 
-drop table if exists test;
-
-create table test (
-    id integer not null primary key,
-    stuff text  
+CREATE TABLE IF NOT EXISTS users (
+	userId INTEGER NOT NULL PRIMARY KEY,
+	username VARCHAR(20),
+	pword VARCHAR(16),
+	fname VARCHAR(30),
+	lname VARCHAR(50),
+	age INTEGER,
+	email VARCHAR(50),
+	ph_Num INTEGER,
+	avatarId INTEGER,
+	country VARCHAR(3)
 );
 
-insert into test (stuff) values
-    ('Things'),
-    ('More things');
+CREATE TABLE IF NOT EXISTS socialMedia (
+    userId INTEGER NOT NULL PRIMARY KEY,
+    faceBook VARCHAR(100),
+    insta VARCHAR(100),
+    youTube VARCHAR(100),
+    twitter VARCHAR(100),
+    linkedIn VARCHAR(100),
+    FOREIGN KEY (userId) REFERENCES users (userId)
+);
+
+CREATE TABLE IF NOT EXISTS articles (
+    articleId INTEGER NOT NULL PRIMARY KEY,
+    content VARCHAR(10000),
+    userId INTEGER,
+    created_At TEXT DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (userId) REFERENCES users (userId)
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+    commentId INTEGER NOT NULL PRIMARY KEY,
+    userId INTEGER,
+    articleId INTEGER,
+    replyTo_Id INTEGER,
+    content VARCHAR(1000),
+    created_At TEXT DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (userId) REFERENCES users (userId),
+    FOREIGN KEY (articleId) REFERENCES articles (articleId),
+    FOREIGN KEY (replyTo_Id) REFERENCES comments (commentId)
+);
