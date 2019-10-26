bfafe23a8c1e2b4bab370dc55a1dae70abc48089
Elliott Nixon <enix002@aucklanduni.ac.nz>
2019-10-26 13:43:31 +1300

    changed password to salt and hashedpassword in usersDb

---
+++ modules/crypto.js
@@ -0,0 +1,43 @@
+// Setup crypto
+const crypto = require('crypto');
+
+/**
+ * generates random string of characters i.e salt
+ * @function
+ * @param {number} length - Length of the random string.
+ */
+const genRandomString = function(length){
+    return crypto.randomBytes(Math.ceil(length/2))
+            .toString('hex') /** convert to hexadecimal format */
+            .slice(0,length);   /** return required number of characters */
+};
+
+/**
+ * hash password with sha512.
+ * @function
+ * @param {string} password - List of required fields.
+ * @param {string} salt - Data to be validated.
+ */
+const sha512 = function(password, salt){
+    const hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+    hash.update(password);
+    const value = hash.digest('hex');
+    return {
+        salt:salt,
+        passwordHash:value  //these two values are to be stored in users in DB
+    };
+};
+
+function saltHashPassword(userpassword) {
+    const salt = genRandomString(16); /** Gives us salt of length 16 */
+    const passwordData = sha512(userpassword, salt);
+    console.log('UserPassword = '+userpassword);
+    console.log('Passwordhash = '+passwordData.passwordHash);
+    console.log('nSalt = '+passwordData.salt);
+}
+
+//Test to show that the hashed and salted password is different each time even for the same password
+saltHashPassword('MYPASSWORD');
+saltHashPassword('MYPASSWORD');
+
+// sha512(userPasswordInput, saltFromDB) compared to storedHashedPword.
