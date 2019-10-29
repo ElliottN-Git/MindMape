dec64457dac88679703c60b74e9707aa48a1a48e
paulwon <ywon112@aucklanduni.ac.nz>
2019-10-30 01:29:36 +1300

    use verifyAuthenticated

--- routes/application-routes.js
+++ routes/application-routes.js
@@ -5,20 +5,11 @@ const testDao = require("../modules/test-dao.js");
 
 const verifyAuthenticated = require("../modules/verify-auth.js");
 
-router.get("/", async function(req, res) {
+router.get("/", verifyAuthenticated, async function(req, res) {
 
     res.locals.title = "TEAM MAPE";
-    //res.locals.allTestData = await testDao.retrieveAllTestData();
 
     res.render("home");
 });
-
-// router.get("/", async function(req, res) {
-
-//     res.locals.title = "TEAM MAPE";
-//     res.locals.allTestData = await testDao.retrieveAllTestData();
-
-//     res.render("home");
-// });
 
 module.exports = router;
