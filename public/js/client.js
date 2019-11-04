eae78b5d9d8c30c5e7e0e3400189ac52dce258ff
Elliott <enix002@aucklanduni.ac.nz>
2019-11-05 00:28:13 +1300

    profanity filter for articles - initial setup

--- public/js/client.js
+++ public/js/client.js
@@ -4,17 +4,17 @@
 const enteredUserName = document.querySelector("#txtUName");
 const usernameTakenSpan = document.querySelector("#usernameTaken");
 const bannedWordSpan = document.querySelector("#bannedWord");
-const submitBtn = document.querySelector("#submitBtn")
+const signupSubmitBtn = document.querySelector("#submitBtn")
 
 //event handler for username entry input
 enteredUserName.addEventListener("keyup", function() {
     checkUniqueUsername(enteredUserName.value);
     if(isProfane(enteredUserName.value)) {
         bannedWordSpan.style.display = "block";
-        submitBtn.disabled = true;
+        signupSubmitBtn.disabled = true;
     } else {
         bannedWordSpan.style.display = "none";
-        submitBtn.disabled = false;
+        signupSubmitBtn.disabled = false;
     }
 });
 
@@ -111,10 +111,10 @@ avatarList.addEventListener("click", function(e) {
     //check password re-enter box on sign-up page
     $('#password, #confirm-password').on('keyup', function () {
         if ($('#password').val() == $('#confirm-password').val()) {
-            submitBtn.disabled = false;
+            signupSubmitBtn.disabled = false;
             $('#message').html('Matching').css('color', 'green');
         } else {
-            submitBtn.disabled = true;
+            signupSubmitBtn.disabled = true;
             $('#message').html('Not Matching').css('color', 'red');
         }
     });
--- public/js/profanity-filter.js
+++ public/js/profanity-filter.js
@@ -1,3 +1,16 @@
+//submit button on write article page
+const postArticleBtn = document.querySelector("#postArticleBtn");
+
+postArticleBtn.addEventListener("click", function() {
+    const titleStr = document.querySelector("#title").value;
+    console.log(titleStr);
+    replaceBannedWords(titleStr);
+
+    const articleContent = document.querySelector("#full-featured-non-premium").value;
+    console.log(articleContent);
+    replaceBannedWords(articleContent);
+});
+
 //Checks the input string against the bannedWords regex 
 // and returns true if any matches are found or false if not.
 function isProfane(string) {
@@ -9,4 +22,10 @@ function isProfane(string) {
         console.log(`Doesn't contain banned words`);
         return false;
     }
+}
+
+function replaceBannedWords(string) {
+    let censoredString = string.replace(bannedWords, "****");
+    console.log(censoredString);
+    return censoredString;
 }
